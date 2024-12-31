9803ff8ad98884d7e8c4df0f1cc1d9080fd582d9
Your Name <your-email@example.com>
2024-12-31 13:22:39 +0200

    הוספת קובץ App.js ושאר הקבצים

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Investments from './components/Investments';
+
+function App() {
+  return (
+    <div className="App">
+      <Investments />
+    </div>
+  );
+}
+
+export default App;
